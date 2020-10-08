3de52a957871ca79e5dca8e03d88b823139ebb04
Lucas362 <lucasvitorifg@gmail.com>
2020-10-08 00:12:24 -0300

    [Feature] Adding alert to winery

--- src/scenes/winery/index.js
+++ src/scenes/winery/index.js
@@ -1,5 +1,8 @@
 import React, {useState} from 'react';
-
+import {
+    Alert,
+    AlertIcon,
+} from "@chakra-ui/core";
 import api from '../../services/api';
 import '../../globals/globalStyle.css';
 
@@ -22,31 +25,46 @@ function Winery(){
         }).then(res => {
             setError('');
             setSuccess('success');
+            setName('');
+            setAddress('');
+            setContractId('');
         }).catch(error => {
             setError('error');
             setSuccess('');
-        })
+        });
     }
 
     return (
         <div className="board">
+            {success === "success" &&
+                <Alert status="success" variant="solid">
+                    <AlertIcon />
+                    Parabéns! Seu cadastro de vinícola foi realizado com sucesso!
+                </Alert>
+            }
+            {error === "error" &&
+                <Alert status="error" variant="solid">
+                    <AlertIcon />
+                    Erro ao cadastrar vinícola
+                </Alert>
+            }
             <div className="inputName">
                 <div className="labelContainer">
                     <p className="labelText">Nome:</p>
                 </div>
-                    <input type="text" maxLength='50' onChange={e => {setName(e.target.value)}}></input>
+                    <input type="text" maxLength='50' value={name} onChange={e => {setName(e.target.value)}}></input>
             </div>
             <div className="inputAddress">
                 <div className="labelContainer">
                     <p className="labelText">Endereço:</p>
                 </div>
-                    <input type="text" maxLength='200' onChange={e => {setAddress(e.target.value)}}></input>
+                    <input type="text" maxLength='200' value={address} onChange={e => {setAddress(e.target.value)}}></input>
             </div>
             <div className="inputId">
                 <div className="labelContainer">
                     <p className="labelText">ID do Contrato:</p>
                 </div>
-                    <input type="text" maxLength='24' onChange={e => {setContractId(e.target.value)}}></input>
+                    <input type="text" maxLength='24' value={contract_id} onChange={e => {setContractId(e.target.value)}}></input>
             </div>
             <div className="buttonArea">
                 <button className="buttonCadastrarVinicola" onClick={() => makeRequest()}>CADASTRAR VINÍCOLA</button>
