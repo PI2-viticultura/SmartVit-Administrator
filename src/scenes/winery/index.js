a94754f3058041689dd0fcfc4c17e87886ee6514
Luciano Santos <luciano_z7@hotmail.com>
2020-10-18 14:28:13 -0300

    [Test] Removing logs

--- src/scenes/winery/index.js
+++ src/scenes/winery/index.js
@@ -14,9 +14,6 @@ function Winery(){
     const [success, setSuccess] = useState("");
 
     const makeRequest = async () => {
-        console.log(name);
-        console.log(address);
-        console.log(contractId);
         await apiWinery.post("/winery", {
             name,
             address,
