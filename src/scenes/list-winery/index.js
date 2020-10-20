256b35bbefee94ea3350672a0f88f4ca74c837d6
Adrianne Alves <driah_as@hotmail.com>
2020-10-19 22:29:17 -0300

    [Feature] Adding list winery filter

--- src/scenes/list-winery/index.js
+++ src/scenes/list-winery/index.js
@@ -8,6 +8,7 @@ import { useHistory } from "react-router-dom";
 
 function ListWinerys() {
   const [data, setData] = useState([]);
+  const [filtereData, setFiltereData] = useState([]);
   const history = useHistory();
   const pushToRegister = async () => {
     history.push("/winery");
@@ -23,9 +24,22 @@ function ListWinerys() {
       }).then((res) => {
         winerys = res.data.filter((element) => typeof element.name === "string");
         setData(winerys);
+        setFiltereData(winerys);
       }).catch((error) => {
       });
   };
+
+  const search = (event)=>{
+    event.preventDefault();
+    const { value } = event.target;
+
+    if (value != ""){
+      let filtered_f = data.filter((element) => (element.name.toUpperCase().includes(value.toUpperCase())) || (element.active && "FUNCIONANDO".includes(value.toUpperCase())) || (!element.active && "DESATIVADA".includes(value.toUpperCase())));
+      setFiltereData(filtered_f);
+    }else{
+      setFiltereData(data);
+    }
+  }
 
   const changeStatus = async (id) => {
     await api.patch("/winery/" + id,
@@ -41,7 +55,7 @@ function ListWinerys() {
 
   useEffect(() => {
     getWinerys();
-  });
+  }, []);
 
 
   const columns = [
@@ -68,12 +82,12 @@ function ListWinerys() {
     <div className="main">
         <Grid className="grid-header" templateColumns="repeat(2, 1fr)" gap={6}>
           <Button className="button-newWinery" variantColor="primary" size="md" w="40%" onClick={() => pushToRegister()}>NOVA VINÍCOLA</Button>
-          <Input className="input-newWinery" placeholder="Basic usage" w="65%" borderColor="#919FA7"/>
+          <Input className="input-newWinery" placeholder="Pesquisar" w="65%" borderColor="#919FA7" onChange={(e) => search(e)}/>
         </Grid>
       <div className="winerys">
         <DataTable
           columns={columns}
-          data={data}
+          data={filtereData}
           defaultSortField="name"
           pagination={true}
         />
