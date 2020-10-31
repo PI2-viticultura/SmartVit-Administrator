65ec6c9481833bd24841c2892e5791312d413e2c
Lucas362 <lucasvitorifg@gmail.com>
2020-10-30 23:27:14 -0300

    [Fix] Fixing api

--- src/services/api-winery.js
+++ src/services/api-winery.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const baseUrl = process.env.REACT_APP_URL_WINERY;
 
 const apiWinery =  axios.create({
-    baseURL: 'https://smartvit-winery-dev.herokuapp.com/',
+    baseURL: baseUrl,
 });
 
 export default apiWinery;
